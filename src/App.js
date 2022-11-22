2f072409d9fb0aa255571c130339de6c2e588ed2
Aarish Ali <63769815+arishali123@users.noreply.github.com>
2022-11-23 00:16:19 +0530

    change in library and design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./header/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
